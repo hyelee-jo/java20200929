01fad2a5a1ba5f4afad06b3f0fd67c9b0798bef3
harbor_@naver.com <harbor_@naver.com>
2020-10-08 16:43:44 +0900

    20201008

--- src/chap04/exercises/Exercise04.java
+++ src/chap04/exercises/Exercise04.java
@@ -1,21 +1,19 @@
 package chap04.exercises;
 
 public class Exercise04 {
-	public static void main(String[] args) {
-		int sum=0;
+public static void main(String[] args) {
+		
 		while (true) {
-			int num1 = (int) (Math.random() * 6) + 1;
-			int num2 = (int) (Math.random() * 6) + 1;
-			System.out.println("(" + num1 + " , " + num2 +")");
-			sum = num1 + num2;
-			if (sum == 5) {
-				System.out.println("");
-				
-				System.exit(0);
-					
+			int dice1 = (int) (Math.random() * 6) +1;
+			int dice2 = (int) (Math.random() * 6) +1;
+			
+			System.out.println("(" + dice1 + ", " + dice2 + ")");
+			
+			if ((dice1 + dice2) == 5) {
+				break;
 			}
 		}
-			
 	}
+	
 
 }
