f01e363d45ef79d40c64951c09f129cf8b57691e
harbor_@naver.com <harbor_@naver.com>
2020-10-19 17:38:32 +0900

    201019

---
+++ src/chap09/textbook/exercises/p05/Anonymous.java
@@ -0,0 +1,18 @@
+package chap09.textbook.exercises.p05;
+
+public class Anonymous {
+	Vehicle field =
+		
+	}
+			
+	void method1() {
+		Vehicle localVar =
+				
+		localVar.run();		
+	}
+	
+	void method2(Vehicle v) {
+		v.run();
+
+}
+}
---
+++ src/chap09/textbook/exercises/p05/Vehicle.java
@@ -0,0 +1,6 @@
+package chap09.textbook.exercises.p05;
+
+public interface Vehicle {
+	public void run();
+
+}
