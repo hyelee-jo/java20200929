0798171f701a6c7fca91ee7e3f7752cbab6d37f2
harbor_@naver.com <harbor_@naver.com>
2020-10-23 17:08:57 +0900

    201023

---
+++ src/chap13/textbook/exercises/p02/Container.java
@@ -0,0 +1,15 @@
+package chap13.textbook.exercises.p02;
+
+public class Container<T> {
+	private T value;
+
+	public void set(T value) {
+		this.value = value;
+	}
+
+	public T get() {
+		
+		return value;
+	}
+
+}
