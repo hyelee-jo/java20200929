291a7be91a337e6f9cff8ad5a60407b8b151687a
harbor_@naver.com <harbor_@naver.com>
2020-12-07 17:19:04 +0900

    201207

--- src/codingbat/String1.java
+++ src/codingbat/String1.java
@@ -1,9 +1,229 @@
 package codingbat;
 
 public class String1 {
+	public String helloName(String name) {
+		return "Hello " + name + "!";
+	}
+
+	public String makeAbba(String a, String b) {
+		return a + b + b + a;
+	}
+
+	public String makeTags(String tag, String word) {
+		return "<" + tag + ">" + word + "</" + tag + ">";
+	}
+
+	public String makeOutWord(String out, String word) {
+		return out.substring(0, 2) + word + out.substring(2);
+	}
+
+	public String extraEnd(String str) {
+		int len = str.length();
+		String last = str.substring(len - 2);
+
+		return last + last + last;
+	}
+
+	public String firstTwo(String str) {
+		int len = str.length();
+
+		if (len > 2) {
+			return str.substring(0, 2);
+		}
+
+		return str;
+	}
+
+	public String firstHalf(String str) {
+		int len = str.length();
+		return str.substring(0, len / 2);
+	}
+
+	public String withoutEnd(String str) {
+
+		return str.substring(1, str.length() - 1);
+	}
+
+	public String comboString(String a, String b) {
+		int lena = a.length();
+		int lenb = b.length();
+
+		if (lena < lenb) {
+			return a + b + a;
+		} else {
+			return b + a + b;
+		}
+	}
+
+	public String nonStart(String a, String b) {
+		return a.substring(1) + b.substring(1);
+	}
+
+	public String left2(String str) {
+		return str.substring(2) + str.substring(0, 2);
+	}
+
+	public String right2(String str) {
+		int len = str.length();
+
+		return str.substring(len - 2) + str.substring(0, len - 2);
+	}
+
+	public String theEnd(String str, boolean front) {
+		String res = "";
+
+		if (front) {
+			res = str.substring(0, 1);
+		} else {
+			res = str.substring(str.length() - 1);
+		}
+
+		return res;
+	}
+
+	public String withouEnd2(String str) {
+		if (str.length() < 3) {
+			return "";
+		} else {
+			return str.substring(1, str.length() - 1);
+		}
+	}
+
+	public String middleTwo(String str) {
+		int mid = str.length() / 2;
+		return str.substring(mid - 1, mid + 1);
+	}
+
+	public boolean endsLy(String str) {
+		return str.endsWith("ly");
+	}
+	
 	public String nTwice(String str, int n) {
 		  return str.substring(0, n) + str.substring(str.length()-n);
 		}
 
+	public String twoChar(String str, int index) {
+		if (index + 2 > str.length() || index < 0) {
+			return str.substring(0, 2);
+		}
+
+		return str.substring(index, index + 2);
+	}
+
+	public String middleThree(String str) {
+		int mid = str.length() / 2;
+
+		return str.substring(mid - 1, mid + 2);
+	}
+
+	public boolean hasBad(String str) {
+		int idx = str.indexOf("bad");
+
+		return idx == 0 || idx == 1;
+	}
+
+	public String atFirst(String str) {
+		while (str.length() < 2) {
+			str += "@";
+		}
+		return str.substring(0, 2);
+	}
+
+	public String lastChars(String a, String b) {
+		a = at(a);
+		b = at(b);
+
+		int lena = a.length();
+		int lenb = b.length();
+
+		return a.substring(0, 1) + b.substring(lenb - 1);
+	}
+
+	private String at(String str) {
+		while (str.length() < 1) {
+			str += "@";
+		}
+		return str;
+	}
+
+	public String conCat(String a, String b) {
+		int lena = a.length();
+		int lenb = b.length();
+
+		if (lena > 0 && lenb > 0) {
+			if (a.charAt(lena - 1) == b.charAt(0)) {
+				b = b.substring(1);
+			}
+		}
+		return a + b;
+	}
+
+	public String lastTwo(String str) {
+
+		int len = str.length();
+
+		if (len < 2) {
+			return str;
+		}
+
+		char last1 = str.charAt(len - 2);
+		char last2 = str.charAt(len - 1);
+
+		return str.substring(0, len - 2) + last2 + last1;
+	}
+
+	public String seeColor(String str) {
+		if (str.startsWith("red")) {
+			return "red";
+		}
+
+		if (str.startsWith("blue")) {
+			return "blue";
+		}
+
+		return "";
+	}
+
+	public boolean frontAgain(String str) {
+		int len = str.length();
+
+		if (len < 2) {
+			return false;
+		}
+
+		String front = str.substring(0, 2);
+		String end = str.substring(len - 2);
+
+		return front.equals(end);
+
+	}
+
+	public String minCat(String a, String b) {
+		int lena = a.length();
+		int lenb = b.length();
+
+		int min = (lena < lenb) ? lena : lenb;
+
+		return a.substring(lena - min) + b.substring(lenb - min);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
---
+++ src/codingbat/WarmUp2.java
@@ -0,0 +1,228 @@
+package codingbat;
+
+public class WarmUp2 {
+	public String stringTimes(String str, int n) {
+		String res = "";
+
+		for (int i = 0; i < n; i++) {
+			res += str;
+		}
+
+		return res;
+	}
+
+	public String frontTimes(String str, int n) {
+		String res = "";
+
+		String front = str;
+
+		if (str.length() > 3) {
+			front = str.substring(0, 3);
+		}
+
+		for (int i = 0; i < n; i++) {
+			res += front;
+		}
+
+		return res;
+	}
+
+	int countXX(String str) {
+		int cnt = 0;
+		int len = str.length();
+
+		// String sub = str.substring(0, 2);
+		// str.substring(len-2, len);
+
+		for (int i = 0; i <= len - 2; i++) {
+			String sub = str.substring(i, i + 2);
+			if (sub.equals("xx")) {
+				cnt++;
+			}
+		}
+
+		return cnt;
+	}
+
+	boolean doubleX(String str) {
+		int xinx = str.indexOf("x");
+		int xxinx = str.indexOf("xx");
+
+		if (xinx == -1) {
+			return false;
+		}
+
+		if (xinx == xxinx) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public String stringBits(String str) {
+		String res = "";
+
+		for (int i = 0; i < str.length(); i += 2) {
+			res += str.charAt(i);
+		}
+
+		return res;
+	}
+
+	public String stringSplosion(String str) {
+		String res = "";
+
+		for (int i = 1; i <= str.length(); i++) {
+			res += str.substring(0, i);
+		}
+
+		return res;
+	}
+
+	public int last2(String str) {
+		int len = str.length();
+		if (len >= 2) {
+			int cnt = 0;
+			String last2 = str.substring(len - 2, len);
+			for (int i = 0; i < len - 1; i++) {
+				String sub = str.substring(i, i + 2);
+				if (sub.equals(last2)) {
+					cnt++;
+				}
+			}
+			return cnt - 1;
+		}
+		return 0;
+	}
+
+	public int arrayCount9(int[] nums) {
+		int cnt = 0;
+		for (int n : nums) {
+			if (n == 9) {
+				cnt++;
+			}
+		}
+
+		return cnt;
+	}
+
+	public boolean arrayFront9(int[] nums) {
+		for (int i = 0; i < nums.length && i < 4; i++) {
+			if (nums[i] == 9) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public boolean array123(int[] nums) {
+		for (int i = 1; i < nums.length - 1; i++) {
+			if (nums[i] == 2) {
+				if (nums[i - 1] == 1 && nums[i + 1] == 3) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	public int stringMatch(String a, String b) {
+		int lena = a.length();
+		int lenb = b.length();
+
+		int loop = Math.min(lena, lenb) - 1;
+
+		int cnt = 0;
+		for (int i = 0; i < loop; i++) {
+			String suba = a.substring(i, i + 2);
+			String subb = b.substring(i, i + 2);
+
+			if (suba.equals(subb)) {
+				cnt++;
+			}
+		}
+		return cnt;
+	}
+
+	public String stringX(String str) {
+		String res = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			if (i == 0 || i == str.length() - 1) {
+				res += c;
+				continue;
+			}
+			if (c != 'x') {
+				res += c;
+			}
+		}
+
+		return res;
+	}
+
+	public String altPairs(String str) {
+		String res = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			if ((i % 4 == 0) || (i - 1) % 4 == 0) {
+				res += str.charAt(i);
+			}
+		}
+
+		return res;
+	}
+
+	public String stringYak(String str) {
+		return str.replace("yak", "");
+	}
+
+	public int array667(int[] nums) {
+		int len = nums.length;
+
+		int count = 0;
+		for (int i = 0; i < len - 1; i++) {
+			if (nums[i] == 6) {
+				if (nums[i + 1] == 6 || nums[i + 1] == 7) {
+					count++;
+				}
+			}
+		}
+
+		return count;
+
+	}
+
+	public boolean noTriples(int[] nums) {
+		int last = nums.length - 2;
+
+		for (int i = 0; i < last; i++) {
+			if (nums[i] == nums[i + 1] && nums[i] == nums[i + 2]) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public boolean has271(int[] nums) {
+		// 2, 7, 1
+		// -1 ~ 3
+
+		// n, n+5, (n-3 ~ n+1)
+
+		for (int i = 0; i < nums.length - 2; i++) {
+			if (nums[i + 1] == nums[i] + 5) {
+				if (nums[i + 2] >= nums[i] - 3 && nums[i + 2] <= nums[i] + 1) {
+
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+}
