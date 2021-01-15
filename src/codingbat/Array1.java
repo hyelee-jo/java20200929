e4b29bd0b51e07e6ee6e014bd52a497f84dad354
harbor_@naver.com <harbor_@naver.com>
2021-01-15 12:29:24 +0900

    210115

---
+++ src/codingbat/Array1.java
@@ -0,0 +1,16 @@
+package codingbat;
+
+public class Array1 {
+	public boolean firstLast6(int[] nums) {
+		return nums[0] == 6 || nums[nums.length - 1] == 6;
+
+	}
+
+	public boolean sameFirstLast(int[] nums) {
+		if (nums.length > 0) {
+			return nums[0] == nums[nums.length - 1];
+		}
+
+		return false;
+	}
+}
